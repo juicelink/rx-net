f288356c523ce6c50ee383a3ea3ba74fdd3b52cc
OREDIS-VP\jlanot <jlanot@veepee.com>
2022-06-29 10:45:11 +0200

    more

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -41,15 +41,16 @@ public class Tests : BaseTests
         var random = new Random();
         async Task<string> Transform(int id)
         {
-            ///
+            Logger.LogTrace("start task for {id}", id);
             await Task.Delay(random.Next(200, 1000));
+            Logger.LogTrace("end task for {id}", id);
             return id.ToString();
         }
 
         IObservable<string> TransfromObservable (int id)
             => Observable.FromAsync(() => Transform(id));
 
-        var end = await CreateSample(10).Select(v => TransfromObservable(v)).Merge(5)
+        var end = await CreateSample(10).SelectMany(v => TransfromObservable(v))
             .Do(str => Logger.LogInformation(str));
     }
 
@@ -70,6 +71,98 @@ public class Tests : BaseTests
         await Observable.Range(1, 10)
             .SelectManyOrdered(TransfromObservable, 50)
             .Do(e => Logger.LogWarning("event output {id}", e));
+    }
+
+    [Fact]
+    public async Task TestLatestFrom()
+    {
+        var leftEvents =
+            GenerateDelayedEvents(10)
+                .Do(i => Logger.LogInformation("left {i}", i));
+
+        var rightEvents =
+            GenerateDelayedEvents(10)
+                .Do(i => Logger.LogInformation("right {i}", i));
+
+        await leftEvents.WithLatestFrom(rightEvents)
+            .Do(v => Logger.LogWarning("left: {left} right : {right}", v.First, v.Second));
+    }
+
+    [Fact]
+    public async Task TestCombineLatest()
+    {
+        var leftEvents =
+            GenerateDelayedEvents(10)
+                .Do(i => Logger.LogInformation("left {i}", i));
+
+        var rightEvents =
+            GenerateDelayedEvents(10)
+                .Do(i => Logger.LogInformation("right {i}", i));
+
+        await leftEvents.CombineLatest(rightEvents)
+            .Do(v => Logger.LogWarning("left: {left} right : {right}", v.First, v.Second));
+    }
+
+    [Fact]
+    public async Task TestJoin()
+    {
+        var left = CreateSample(10).Select(v => $"l{v}")
+            .Publish().RefCount();
+
+        var right = CreateSample(10).Select(v => $"r{v}")
+            .Publish().RefCount();
+
+        await left
+            .Join(right,
+                _ => left,
+                _ => left,
+                (l, r) => $"{l} {r}")
+            .Do(v => Logger.LogInformation(v));
+    }
+
+    [Fact]
+    public async Task TestExpand()
+    {
+        IObservable<(int nextPage, int[] data)> queryData(int page)
+        {
+            int[] getData(int p) =>
+                page < 10 ? Enumerable.Range(1, 5).ToArray() : Array.Empty<int>();
+
+            var data = getData(page);
+
+            return data.Any()
+                ? Observable.Return((page + 1, Enumerable.Range(1, 5).ToArray()))
+                : Observable.Empty<(int, int[])>();
+        }
+
+        await queryData(1).Expand(v => queryData(v.nextPage))
+            .SelectMany(d => d.data)
+            .Do(v => Logger.LogInformation("{v}", v));
+    }
+
+    [Fact]
+    public async Task TestBuffer()
+    {
+        await CreateSample(50).Buffer(TimeSpan.FromSeconds(1), 5)
+            .Do(l => Logger.LogInformation("{l}", l));
+    }
+
+    [Fact]
+    public async Task TestConcat2()
+    {
+        var random = new Random();
+        async Task<string> Transform(int id)
+        {
+            ///
+            await Task.Delay(random.Next(200, 1000));
+            return id.ToString();
+        }
+
+        IObservable<string> TransfromObservable(int id)
+            => Observable.FromAsync(() => Transform(id));
+
+        var end = await CreateSample(10).Select(v => TransfromObservable(v)).Concat()
+            .Do(str => Logger.LogInformation(str));
     }
 
     [Fact]
@@ -101,5 +194,12 @@ public class Tests : BaseTests
                 v => Observable.Return(v)
                     .Delay(TimeSpan.FromMilliseconds(random.Next(100, 500))))
             .Concat();
+    }
+
+    private static IObservable<int> GenerateDelayedEvents(int nb)
+    {
+        var random = new Random();
+        return Observable.Range(1, nb)
+            .Select(i => Observable.Return(i).Delay(TimeSpan.FromMilliseconds(random.Next(500, 2000)))).Concat();
     }
 }
